c2bd3fa177e715dd914bb0885498ebb973e8105f
Ygor Torres <88403929+ygortg@users.noreply.github.com>
2022-11-23 17:58:00 -0300

    Updates

---
+++ precache-manifest.d57b1e9d8b67f9666a8da2be26043099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d667cde3aa792a8ff6b8",
+    "url": "/sorveteria-artesanal/static/css/main.f9af2963.chunk.css"
+  },
+  {
+    "revision": "d667cde3aa792a8ff6b8",
+    "url": "/sorveteria-artesanal/static/js/main.d667cde3.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/sorveteria-artesanal/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "104f2367f3edba0d4ff5",
+    "url": "/sorveteria-artesanal/static/js/2.104f2367.chunk.js"
+  },
+  {
+    "revision": "7488bc2c4bc16e43d8e8",
+    "url": "/sorveteria-artesanal/static/js/runtime~main.7488bc2c.js"
+  },
+  {
+    "revision": "fa7d582dfbdd40fbfd9bbd0b5f7b8f41",
+    "url": "/sorveteria-artesanal/index.html"
+  }
+];
